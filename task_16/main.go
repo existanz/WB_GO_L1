e66596158087cf7b91be599259d9a126766ae914
EXisTAnZ <ozim_ssspam@mail.ru>
2024-12-06 19:08:44 +0300

    fix: add generics to quicksort

--- task_16/main.go
+++ task_16/main.go
@@ -5,6 +5,7 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 )
 
@@ -12,16 +13,29 @@ func main() {
 	nums := []int{1, 3, 3, 10, 5, 1, 7, 12, 9, 10}
 	quickSort(nums)
 	fmt.Println(nums)
+
+	strs := []string{"apple", "zuccini", "orange", "eggplant", "banana"}
+	quickSort(strs)
+	fmt.Println(strs)
 }
 
-func quickSort(arr []int) {
+func quickSort[T cmp.Ordered](arr []T) {
 	if len(arr) <= 1 {
 		return
 	}
-	left, right := 0, len(arr)-1
-	pivot := (arr[left] + arr[right]) / 2
 
-	for left < right {
+	if len(arr) == 2 {
+		if arr[0] <= arr[1] {
+			return
+		}
+		arr[0], arr[1] = arr[1], arr[0]
+		return
+	}
+
+	left, right := 0, len(arr)-1
+	pivot := arr[(left+right)/2]
+
+	for {
 		for arr[left] < pivot {
 			left++
 		}
