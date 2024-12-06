afeccb2ff79feaa14bc52d6b0f58a62dfbfcf215
EXisTAnZ <ozim_ssspam@mail.ru>
2024-12-06 21:39:36 +0300

    feat: solve task 19

---
+++ task_19/main.go
@@ -0,0 +1,23 @@
+/*
+Разработать программу, которая переворачивает подаваемую на ход строку
+(например: «главрыба — абырвалг»). Символы могут быть unicode.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(Reverse("главрыба 😜"))
+}
+
+func Reverse(s string) string {
+	runes := []rune(s)
+	l, r := 0, len(runes)-1
+	for l < r {
+		runes[l], runes[r] = runes[r], runes[l]
+		l++
+		r--
+	}
+	return string(runes)
+}
