a1ffbb27588d55121fb9343020dde0cbf2d8899e
EXisTAnZ <ozim_ssspam@mail.ru>
2024-12-09 12:47:28 +0300

    fix: create separate funcs for set unset and flip bit task 08

--- task_08/main.go
+++ task_08/main.go
@@ -9,20 +9,23 @@ import (
 )
 
 func main() {
-	var num int64
-	fmt.Println("Input the number in range [-9223372036854775808, 9223372036854775807]")
-	fmt.Scan(&num)
+	num := int64(185)
+	fmt.Printf("%v \t --> \t %b\n", num, num)
+	fmt.Printf("%v \t --> \t %b\n", setBit(num, 1), setBit(num, 1))
+	fmt.Printf("%v \t --> \t %b\n", unsetBit(num, 7), unsetBit(num, 7))
+	fmt.Printf("%v \t --> \t %b\n", unsetBit(num, 3), unsetBit(num, 3))
+	fmt.Printf("%v \t --> \t %b\n", flipBit(num, 4), flipBit(num, 4))
+	fmt.Printf("%v \t --> \t %b\n", flipBit(num, 200), flipBit(num, 200)) // выход за диапазон, но конструкция 1 << n при n > 63 вернёт 0, поскольку такого бита не существует
+}
 
-	var i uint8
-	fmt.Println("Input the index in range [0, 62]")
-	fmt.Scan(&i)
+func setBit(num int64, i uint8) int64 {
+	return num | (1 << i)
+}
 
-	if i == 63 {
-		fmt.Println("You can't set the MSB")
-	} else if i > 63 {
-		fmt.Println("Index is out of range")
-	}
+func unsetBit(num int64, i uint8) int64 {
+	return num & ^(1 << i)
+}
 
-	num ^= 1 << i
-	fmt.Println(num)
+func flipBit(num int64, i uint8) int64 {
+	return num ^ (1 << i)
 }
